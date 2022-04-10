80d5a56f61005bebead30daa1cec2881fc9d8bde
Jake Dennison <srt4rulez@gmail.com>
2022-04-10 15:20:02 -0400

    Add durationInSeconds, deprecated length

--- src/resolvers.js
+++ src/resolvers.js
@@ -41,10 +41,16 @@ const resolvers = {
       return dataSources.trackAPI.getAuthor(authorId);
     },
 
+    durationInSeconds: ({ length }) => length,
+
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
   },
+  Module: {
+    durationInSeconds: ({ length }) => length,
+  },
+
 };
 
 module.exports = resolvers;
